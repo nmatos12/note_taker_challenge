f348687bf4832cbc0096d2be37ab2abfa05abeee
unknown <nataliematos12@gmail.com>
2023-05-10 02:07:12 -0400

    added server and api routes as well as server.js, issue with loading css and js that I am unable to fix will try again but wanted to commit

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
-
 const express = require('express');
 const fs = require('fs');
-
-const PORT = process.env.PORT || 3001;
+const apiRoutes = require('./routes/api_routes');
+const htmlRoutes = require('./routes/html_routes');
 
 const app = express();
 
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => console.log('Server listening on PORT %s', PORT));
